4c3880f3567e9dc6ea6e5f15df2d3912c320584b
choisuhyeok <suhyeok00@naver.com>
2025-02-15 23:50:50 +0900

    2025.02.15 ver

--- src/Component/IntroduceCard.jsx
+++ src/Component/IntroduceCard.jsx
@@ -6,16 +6,23 @@ function IntroduceCard() {
     return (
         <div>
             <div className="introduce_card">
-                <ul>
+                <div className="introduce_card_side_bar"></div>
+                <ul className="introduce_card_main">
                     <li>
-                        <ul>
-                            <li>최수혁</li>
+                        <ul className="introduce_card_main_title">
+                            <li>
+                                <h1>최수혁</h1>
+                            </li>
                             <li>GDG on Campus Front-end 유닛장</li>
                         </ul>
                     </li>
-                    <li>전화번호 ?</li>
-                    <li>메일 ?</li>
-                    <li>깃 주소 ?</li>
+                    <li className="introduce_card_main_body">
+                        <ul>
+                            <li>전화번호 ?</li>
+                            <li>메일 ?</li>
+                            <li>깃 주소 ?</li>
+                        </ul>
+                    </li>
                 </ul>
             </div>
         </div>
